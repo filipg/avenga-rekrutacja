04f864f7ec103e639c4d88fa12cd40e169618aa9
filipg <gruczka.f@gmail.com>
2020-06-19 18:24:21 +0200

    added special-offer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { SpecialOfferComponent } from './special-offer/special-offer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainNavComponent
+    MainNavComponent,
+    SpecialOfferComponent
   ],
   imports: [
     BrowserModule,
