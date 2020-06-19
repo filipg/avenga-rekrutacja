05f60b32318f708d7951e25ca3de38156faa90d1
filipg <gruczka.f@gmail.com>
2020-06-19 20:02:56 +0200

    added base vod component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,12 +13,14 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { SpecialOfferComponent } from './special-offer/special-offer.component';
+import { VodComponent } from './vod/vod.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainNavComponent,
-    SpecialOfferComponent
+    SpecialOfferComponent,
+    VodComponent
   ],
   imports: [
     BrowserModule,
